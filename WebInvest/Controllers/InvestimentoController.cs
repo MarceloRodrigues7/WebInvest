a9fbd9868c65a0e30b436b70ecb24449729859d3
Marcelo Rodrigues <marcelovvrodrigues@gmail.com>
2022-02-01 12:06:19 -0300

    implementado investimentos usuarios de databaseLib

--- WebInvest/Controllers/InvestimentoController.cs
+++ WebInvest/Controllers/InvestimentoController.cs
@@ -1,4 +1,5 @@
 ﻿using Dapper;
+using DatabaseLib.Repository;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -14,11 +15,14 @@ namespace WebInvest.Controllers
 {
     public class InvestimentoController : Controller
     {
+        private readonly IInvestimentosUsuarioRepository _investimentosUsuarioRepository;
         private readonly ILogger<InvestimentoController> _logger;
+
         private readonly string _connectionString;
 
         public InvestimentoController(ILogger<InvestimentoController> logger, IConfiguration configuration)
         {
+            _investimentosUsuarioRepository = new InvestimentosUsuarioRepository();
             _logger = logger;
             _connectionString = configuration.GetConnectionString("DataServer");
         }
@@ -26,17 +30,8 @@ namespace WebInvest.Controllers
         [Authorize]
         public IActionResult Index()
         {
-            using (var connection = new SqlConnection(_connectionString))
-            {
-                connection.Open();
-                var query = @"SELECT Acoes.Id,Acoes.sigla,InvestimentosUsuarios.Quantidade,Acoes.valorAtual,
-                              Acoes.dataAtualizacao,InvestimentosUsuarios.Quantidade*Acoes.valorAtual AS 'AoVender'
-                              FROM InvestimentosUsuarios INNER JOIN Acoes ON(InvestimentosUsuarios.IdAcao=Acoes.Id)
-                              WHERE InvestimentosUsuarios.IdUsuario=@idUsuario";
-                var data = connection.Query<InvestimentoUsuario>(query, new { idUsuario = User.Identity.Name });
-                connection.Close();
-                return View(data);
-            };
+            var data = _investimentosUsuarioRepository.GetInvestimentoUsuarios(long.Parse(User.Identity.Name));
+            return View(data);
         }
     }
 }
